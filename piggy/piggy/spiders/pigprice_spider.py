33c30484d22127fce064261760e34021716dbbb2
Lin Weiwei <linweiwei@elink-cn.com>
2018-12-20 16:33:38 +0800

    First spider of pig prices
    One can crawl pig prices of all provinces.

---
+++ piggy/piggy/spiders/pigprice_spider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class PigpriceSpider(scrapy.Spider):
+    name = "pigprice"
+    start_urls = [
+        'http://hqb.nxin.com/hqb/queryPigPrice.shtml',
+    ]
+
+    def parse(self, response):
+        for district in response.css('table tbody tr'):
+            yield {
+                'districtName': district.css("td::text")[0].extract(),
+                'districtData': district.css("td span::text").extract(),
+                'priceDate': response.css("div.pt_tit::text").extract_first(),
+            }
